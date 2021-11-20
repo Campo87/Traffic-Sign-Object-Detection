d3fbfb89a9644d1c14ac6b3257be00bab8f3de5b
Campo87 <jamesestarks87@gmail.com>
2021-11-20 02:22:20 -0600

    Resize script working and resized images generated.

--- data/training/resize.py
+++ data/training/resize.py
@@ -1,6 +1,11 @@
+import os
 import cv2
 import glob
-import numpy as np
 
-filenames = [img for img in glob.glob("/exclusionary/*.png")]
-print(f"test\n {filenames}")
+dirs = ['exclusionary', 'lane-usage', 'one-way', 'railroad',
+        'school', 'speedlimit', 'stop-yield', 'warning']
+
+for dir in dirs:
+    images = [[cv2.imread(file), os.path.basename(file)] for file in glob.glob(f"original/{dir}/*.png")]
+    for image in images:
+        cv2.imwrite(f"{dir}/{image[1]}", cv2.resize(image[0], (32, 32)))
