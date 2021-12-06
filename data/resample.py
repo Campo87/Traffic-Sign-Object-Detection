cbbb1a2a43152df5399b6e5d1829410e1e3c3f2e
Campo87 <jamesestarks87@gmail.com>
2021-12-06 14:27:10 -0600

    Updated image data loading process

--- data/resample.py
+++ data/resample.py
@@ -6,6 +6,9 @@ import numpy as np
 import tensorflow as tf
 from keras import layers
 
+IMG_HEIGHT = 32
+IMG_WIDTH = 32
+
 # Update this if new sign categorgies are added
 dirs = ['exclusionary', 'lane-usage', 'one-way', 'railroad',
         'school', 'speedlimit', 'stop-yield', 'warning']
@@ -19,16 +22,19 @@ for dir in dirs:
 
 # Setup data augementation model
 data_augmentation = tf.keras.Sequential([
-        layers.Resizing(64, 64),
-        layers.RandomContrast(factor=0.5),
+        layers.RandomContrast(factor=0.8),
         layers.RandomRotation(factor=0.02),
         layers.RandomHeight(factor=0.2),
-        layers.RandomWidth(factor=0.2)
+        layers.RandomWidth(factor=0.2),
+        layers.Resizing(32, 32)
 ])
 
 # Augment the data 'n save it
 for image in images:
         # Make 20 augmented copies of each type of sign
+        path = f"images/{image[1][:-4]}"
+        if not os.path.exists(path):
+                os.mkdir(path)
         for idx in range(0, 20):
                 result = np.array(data_augmentation(image[0]))
-                cv2.imwrite(f"images/{image[1][:-4]}_{idx}.png", result)
+                cv2.imwrite(f"{path}/{image[1][:-4]}_{idx}.png", result)
--- project-sign-detection.py
+++ project-sign-detection.py
@@ -2,6 +2,7 @@ import glob
 import os
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -9,47 +10,57 @@ from keras.preprocessing.image import ImageDataGenerator
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import datasets, layers, models
+from tensorflow.python.ops.gen_math_ops import mod
+from tensorflow.python.keras import Sequential
+from tflite_model_maker import model_spec
+from tflite_model_maker import object_detector
 
+IMG_HEIGHT = 32
+IMG_WIDTH = 32
+CHANNELS = 3
 
 class SignDetection():
-    df = pd.DataFrame(columns=['Image', 'Label'])
-    labels = list()
-    num_classes = 0
+    num_classes = 42
     def __init__(self):
         self.read_training_data()
     
     def read_training_data(self):
-        images = [[cv2.imread(file), os.path.basename(file)] for file in glob.glob(f"data/images/*.png")]
-        for image in images:
-            self.df = self.df.append({'Image': image[0],
-                           'Label': image[1][:-6]}, ignore_index=True)
-
-        le = preprocessing.LabelEncoder()
-        le.fit(self.df['Label'])
-        self.df['Label_Encoded'] = le.transform(self.df['Label'])
-        self.labels = le.inverse_transform(self.df['Label_Encoded'])
-        self.labels = list(dict.fromkeys(self.labels))
-        self.num_classes = len(self.labels)
-
-        X = self.df.iloc[:, [0]]
-        y = self.df.iloc[:, [1]]
-
-        self.X_train, self.X_validate, self.y_train, self.y_validate = train_test_split(X, y, test_size=0.2,
-                                                                                random_state=1, stratify=y)
-
-
-    def display_images(self):
-        for idx, row in self.df.iterrows():
-            cv2.imshow(winname=self.labels[row['Label_Encoded']], mat=row['Image'])
-            cv2.waitKey(0)
+        data_dir = 'data/images'
+        self.train_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=(IMG_HEIGHT, IMG_WIDTH),
+            batch_size=32
+        )
+        
+        self.val_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="validation",
+            seed=123,
+            image_size=(IMG_HEIGHT, IMG_WIDTH),
+            batch_size=32
+        )
+        self.class_names = self.train_ds.class_names
+        #plt.figure(figsize=(10, 10))
+        #for images, labels in self.train_ds.take(1):
+        #    for i in range(9):
+        #        ax = plt.subplot(3, 3, i + 1)
+        #        plt.imshow(images[i].numpy().astype("uint8"))
+        #        plt.title(class_names[labels[i]])
+        #        plt.axis("off")
+        #plt.waitforbuttonpress()
     
     def model(self):
-        self.model = tf.keras.Sequential([
-            layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(180, 180, 3)),
+        self.model = Sequential([
+            layers.Rescaling(1./255),
+            layers.Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, CHANNELS)),
             layers.MaxPooling2D(),
-            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
             layers.MaxPooling2D(),
-            layers.Conv2D(64, 3, padding='same', activation='relu'),
+            layers.Conv2D(256, (3, 3), padding='same', activation='relu'),
             layers.MaxPooling2D(),
 
             layers.Flatten(),
@@ -60,21 +71,29 @@ class SignDetection():
         self.model.compile(optimizer='adam',
                            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                            metrics=['accuracy'])
-        
-        #self.X_train =  tf.convert_to_tensor(self.X_train)
-        #self.X_validate =  tf.convert_to_tensor(self.X_validate)
-        self.history = self.model.fit(x=self.X_train, y=self.y_train, epochs=5,
-                                      validation_data=(self.X_validate, self.y_validate))
+        self.history_train = self.model.fit(self.train_ds, epochs=5)
 
+        self.history_val = self.model.evaluate(self.val_ds)
+        self.model.save("model/cnn_model.h5")
+        self.model.save_weights("model/cnn_model_weight.h5")
 
+    def load_model(self):
+        self.model = tf.keras.models.load_model("model/cnn_model.h5")
 
-    def model_create_compile(self):
-        pass
+        image = cv2.resize(cv2.imread('test.png'), (IMG_HEIGHT, IMG_WIDTH))
+        image = np.expand_dims(image, axis=0)
+
+        pred = self.model.predict(image)
+        result = pred.argmax()
+        print(pred)
+        print(pred[0][result])
+        print(self.class_names[result])
 
 
 def main():
     sd = SignDetection()
     sd.model()
+    #sd.load_model()
     del sd
 
 
